70ad02e8c795017a5d06bad5241774edcb76634c
Mauricio Mahmud Sanchez <mauxms1996@gmail.com>
2023-04-16 01:06:50 -0400

    Cleaning

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from ftp_server.server import FTP, FTPConfiguration 
 from ftp_server import commands
 
-config = FTPConfiguration(
-    id='FTP0',
-    host='0.0.0.0',
-    port=7890,
-    root_path='.',
-    welcome_message='Hello World!',
-    commands=commands.ALL
-)
+from os import environ
+import random
 
-FTP(config).run()
+
+def start_ftp_server(config: FTPConfiguration):
+    print(f"Server {config['id']} ejecutandose en {config['host']}:{config['port']}.")
+    FTP(config).run()
+
+
+if __name__ == '__main__':
+
+    ftp_id = environ['ID'] if 'ID' in environ else 'FTP_DEFAULT_ID'
+    start_ftp_server(FTPConfiguration(
+        id=ftp_id,
+        host='0.0.0.0',
+        port= int(environ['PORT']) if 'PORT' in environ else random.randint(5000, 20000),
+        root_path=environ['ROOT_PATH'] if 'ROOT_PATH' in environ else '.',
+        welcome_message=f'Welcome to FTP {ftp_id}!!',
+        commands=commands.ALL
+    ))
