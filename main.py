986d20af24c94ecaeb07ab9846011904db22cd71
Mauricio Mahmud Sanchez <mauxms1996@gmail.com>
2023-04-19 13:39:04 -0400

    Add Help to some arguments.

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ import random
 import argparse
 
 
-def start_ftp_server(config: FTPConfiguration):
-    print(f"Server {config['id']} ejecutandose en {config['host']}:{config['port']}.")
-    FTP(config).run()
 
 
 if __name__ == '__main__':
@@ -21,11 +18,15 @@ if __name__ == '__main__':
                         choices=['ftp', 'proxy', 'analizer'],
                         help='Tipo de servicio a ejecutar')
 
-    parser.add_argument('--id',default='DEFAULT_ID')
-    parser.add_argument('--host',default='0.0.0.0')
-    parser.add_argument('--port',default=str(random.randint(5000, 20000)))
-    parser.add_argument('--root-dir', default='.')
-    parser.add_argument('--welcome-msg', default='Connection Success!')
+    parser.add_argument('--id',default='DEFAULT_ID', help='El id del servidor')
+    parser.add_argument('--host',default='0.0.0.0',
+                        help='El host donde se va a ejecutar.')
+    parser.add_argument('--port',default=str(random.randint(5000, 20000)),
+                        help='El puerto donde se va a ejecutar.')
+    parser.add_argument('--root-dir', default='.',
+                        help='En el caso de ser un FTP, la raiz del almacenamiento.')
+    parser.add_argument('--welcome-msg', default='Connection Success!',
+                        help='El Mensaje de bienvenida')
     args = parser.parse_args()
 
     ID = args.id
@@ -34,22 +35,20 @@ if __name__ == '__main__':
     ROOT_DIR = args.root_dir
     WELCOME_MSG = args.welcome_msg
 
+    ns_utils.ns_register(f"{args.service}_{ID}",HOST,PORT)
+    print(f"Server {ID} ejecutandose en {HOST}:{PORT}.")
     if args.service == 'ftp':
-        ns_utils.ns_register(f"ftp_{ID}",HOST,PORT)
-        start_ftp_server(FTPConfiguration(
+        FTP(FTPConfiguration(
             id=ID,
             host=HOST,
-            port= int(PORT), 
+            port= PORT, 
             root_path=ROOT_DIR,
             welcome_message=WELCOME_MSG,
             commands=commands.ALL
-        ))
+        )).run()
 
     elif args.service == 'proxy':
-        ns_utils.ns_register(f"proxy_{ID}",HOST,PORT)
-        print(f"Server {ID} ejecutandose en {HOST}:{PORT}.")
         Proxy(HOST,PORT).run()
 
     elif args.service == 'analizer':
-        ns_utils.ns_register(f'analizer_{ID}',HOST,PORT)
         Analizer(HOST,PORT,10).run()
