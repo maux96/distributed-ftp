82fe7bbad50c1cba957738855260fdd3e581ac92
rb58853 <rb58853@gmail.com>
2023-07-12 03:23:45 -0400

    sync init

--- coordinator_server/sinc.py
+++ coordinator_server/sinc.py
@@ -14,9 +14,15 @@ class Sinc:
         for dir in recieved_tree.keys():
             if dir not in self.tree:
                 self.coordinator.ftp_tree[dir] = recieved_tree.value()    
+            else:
+                for _,ftps in recieved_tree[dir]:
+                    for ftp in ftps:
+                        if ftp not in self.coordinator.ftp_tree[dir]['ftps']: #TODO Arreglar esto donde se crea el ft_tree
+                            #Si no esta un ftp asociado a este archivo que si esta otra red llamar este
+                            self.coordinator.ftp_tree[dir]['ftps'].append(ftp)
 
         logging.info(str(self.coordinator.id) +
-                     ": sync success: \n" + str(self.tree))
+                     ": sync success: \n" + str(self.coordinator.ftp_tree))
 
     def send_sinc_to(self, socket):
         if socket is None:
@@ -31,6 +37,7 @@ class Sinc:
 
     def sinc_with_leader(self, buffer):
         recieved_tree = json.loads(buffer.decode())
+        #como se esta coordinando con el lider nos interesa que tome exactamente lo que tiene el lider, luego se limpia el tree de este coordinador y se replica exactamente igual al del lider
+        self.coordinator.ftp_tree = {}
         for dir in recieved_tree.keys():
-            if dir not in self.tree:
-                self.coordinator.ftp_tree[dir] = recieved_tree.value()    
+            self.coordinator.ftp_tree[dir] = recieved_tree.value()    
